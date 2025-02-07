c915edd50d156d9f4a3e8cab99505aba11e1dac1
jotavezz <joaovitordossantosmendonca@gmail.com>
2025-02-07 09:14:33 -0300

    alterando valor de limite para 15

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 // let paragrafo = document.querySelector('p');
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 15;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -17,7 +17,7 @@ responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 15');
 }
 exibirMensagemInicial();
 
